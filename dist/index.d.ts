3601ab3c2e68f676ccf829a5c8bd9b1233ca1fb8
Nelo Crypto <nelo.crypto@protonmail.com>
2022-06-03 00:15:21 +0100

    Expose interfaces

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,5 @@
 import EnumPreset from './enums/Preset';
 export { EnumPreset, };
+import IPreset from './interfaces/IPreset';
+import IPresetCategory from './interfaces/IPresetCategory';
+export { IPreset, IPresetCategory };
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,9 @@ import EnumPreset from './enums/Preset'
 export {
     EnumPreset,
 }
+
+// Interfaces
+import IPreset from './interfaces/IPreset'
+import IPresetCategory from './interfaces/IPresetCategory'
+
+export {IPreset, IPresetCategory}
