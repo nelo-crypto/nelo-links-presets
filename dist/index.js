7c57978fa35a47695d548372d71bb59ab3dead21
Nelo Crypto <nelo.crypto@protonmail.com>
2022-06-25 21:16:31 +0100

    - Add dist

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.EnumPreset = void 0;
+exports.EnumBlockExplorer = exports.EnumPreset = void 0;
 // Enums
 const Preset_1 = __importDefault(require("./enums/Preset"));
 exports.EnumPreset = Preset_1.default;
+const BlockExplorer_1 = __importDefault(require("./enums/Blockchain/BlockExplorer"));
+exports.EnumBlockExplorer = BlockExplorer_1.default;
