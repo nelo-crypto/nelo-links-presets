698096ef93a44c175c374e51903d851c9445a42b
Nelo Crypto <nelo.crypto@protonmail.com>
2022-07-03 16:33:10 +0100

    Add Tenset

---
+++ dist/enums/Blockchain/Tenset.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const TENSET = {
+    name: 'Tenset',
+    presets: [
+        {
+            name: 'Tenset.io',
+            url: 'https://www.tenset.io/',
+            description: '',
+        },
+        {
+            name: 'Tenset Infinity Pool',
+            url: 'https://infinity.tenset.io/',
+            description: '',
+        },
+        {
+            name: 'Metahero',
+            url: 'https://metahero.io/',
+            description: '',
+        },
+        {
+            name: 'Everdome',
+            url: 'https://everdome.io/',
+            description: '',
+        },
+    ]
+};
+exports.default = TENSET;
